6d2aef3eae7dfae642310887e72e246a9e1e23c9
mflorencio-inteli <marcos.silva@sou.inteli.edu.br>
2022-08-07 12:38:42 -0300

    included comments & adjusted variables names

--- ethereum/contracts/Lecture.sol
+++ ethereum/contracts/Lecture.sol
@@ -1,22 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+
+//TODO: openzeppelin ERC1155 & Ownable - npm install? qual versao? outro jeito?
+//TODO: ajuste onlyOwner
+pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lecture is ERC1155, Ownable {
-
+contract Lecture is ERC1155 {
     address[] public people;
 
-    constructor(address[] memory _arrayUsers, string memory _ipfsLink) ERC1155(_ipfsLink) {
+    constructor(address[] memory _arrayUsers, string memory _ipfsLink)
+        ERC1155(_ipfsLink)
+    {
         people = _arrayUsers;
-        for (uint i = 0; i >= _arrayUsers.length; i ++){
+        for (uint256 i = 0; i >= _arrayUsers.length; i++) {
             _mint(_arrayUsers[i], 1, _arrayUsers.length, "");
-        }       
+        }
     }
 
-    function burnNFT(address _address) public onlyOwner {
+    function burnNFT(address _address) public {
         _burn(_address, 1, 1);
     }
-   
 }
--- ethereum/contracts/intelifactory.sol
+++ ethereum/contracts/intelifactory.sol
@@ -1,128 +1,169 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
+/*
+InteliFactory contract: master contract of the project. handle deployment of other
+contracts and consolidate data
+    . state variables
+        - owner: EOA, represents Inteli mngmt
+        - students: maps students ID to their hashed ID on the blockchain
+    . methods: create, retrieve and remove Persons
+*/
+
+// TODO: separar contratos em arquivos
 contract InteliFactory {
     address owner;
     mapping(string => address) students;
+    mapping(address => string) wallets;
 
     constructor() {
         owner = msg.sender;
     }
 
-    function CreateStudent(string memory _ra) public {
+    function createStudent(string memory _id) public {
         require(owner == msg.sender);
-        Person person = new Person();
-        students[_ra] = address(person);
+        Person person = new Person(owner);
+        students[_id] = address(person);
+        wallets[address(person)] = _id;
     }
 
-    function GetStudent(string memory _ra) public view returns (address) {
+    function getWallet(string memory _id) public view returns (address) {
         require(owner == msg.sender);
-        return students[_ra];
+        return students[_id];
     }
 
-    function RemoveStudent(string memory _ra) public {
+    function getStudent(address _id) public view returns (string memory) {
         require(owner == msg.sender);
-        delete students[_ra];
+        return wallets[_id];
+    }
 
-        //Chamar burn do Person - Contrato do Paulo
+    function removeStudent(string memory _id) public {
+        require(owner == msg.sender);
+        delete wallets[students[_id]];
+        delete students[_id];
+
+        //TODO: self-destruct de PERSON
     }
 }
 
+/*
+Person contract: manage person profile and track activities
+    . state variables
+        - owner: EOA == Inteli mngmt
+        - checkIn: log check-ins in a given day
+        - checkOut: log check-outs in a given day
+        - activities: log activities done by person
+    . methods: create and retrieve events and register in state variable
+*/
 contract Person {
     address public owner;
-    mapping(string => uint64[]) private checkIn;
-    mapping(string => uint64[]) private checkOut;
+    mapping(string => uint64[]) private campusCheckIn;
+    mapping(string => uint64[]) private campusCheckOut;
     mapping(string => mapping(string => address[])) public activities;
 
-    constructor() {
-        owner = msg.sender;
+    constructor(address _owner) {
+        owner = _owner;
     }
 
-    //Função que registra entrada(dia e tempo) e joga pra variável
-    function newCheckIn(string memory date, uint64 unixTime) public {
+    function newCheckIn(string memory _date, uint64 _unixTime) public {
         require(msg.sender == owner);
-        checkIn[date].push(unixTime);
+        campusCheckIn[_date].push(_unixTime);
     }
 
-    function getCheckIn(string memory key)
+    function getCheckIn(string memory _id)
         public
         view
         returns (uint64[] memory)
     {
-        return checkIn[key];
+        return campusCheckIn[_id];
     }
 
-    //Função que registra saida(dia e tempo) e joga pra variável
-    function newCheckOut(string memory date, uint64 unixTime) public {
+    function newCheckOut(string memory _date, uint64 _unixTime) public {
         require(msg.sender == owner);
-        checkOut[date].push(unixTime);
+        campusCheckOut[_date].push(_unixTime);
     }
 
-    function getCheckOut(string memory key)
+    function getCheckOut(string memory _id)
         public
         view
         returns (uint64[] memory)
     {
-        return checkOut[key];
+        return campusCheckOut[_id];
     }
 
     function newActivity(
-        string memory activityType,
-        string memory date,
-        address activityAdress
+        string memory _activityType,
+        string memory _date,
+        address _activityAdress
     ) public {
-        activities[activityType][date].push(activityAdress);
+        activities[_activityType][_date].push(_activityAdress);
     }
 
-    function viewActivity(string memory activityType, string memory date)
+    function getActivity(string memory _activityType, string memory _date)
         public
         view
         returns (address[] memory)
     {
-        return activities[activityType][date];
+        return activities[_activityType][_date];
     }
 }
 
+/*
+AccessCampus contract: consolidation of Person IDs checking in *OR* out in a given day
+    . state variables
+        - owner: EOA == Inteli mngmt
+        - Access: .sol object = Person ID and moment of entry/exit
+        - entries: log of check-ins in a given day
+        - exits: log of check-outs in a given day
+    . methods: create and retrieve events and register in state variable
+*/
 contract AccessCampus {
+    //TODO: owner
     struct Access {
         address userAddress;
         uint256 time;
     }
-    mapping(string => Access[]) entries;
 
-    mapping(string => Access[]) exits;
+    mapping(string => Access[]) checkIns;
+    mapping(string => Access[]) checkOuts;
 
-    function logsAccess(
-        address userAddress,
-        uint256 time,
-        string memory date
+    function createCheckIn(
+        address _userAddress,
+        uint256 _time,
+        string memory _date
     ) public {
-        Access memory entry = Access({userAddress: userAddress, time: time});
-        entries[date].push(entry);
+        Access memory checkIn = Access({
+            userAddress: _userAddress,
+            time: _time
+        });
+        checkIns[_date].push(checkIn);
     }
 
-    function logExits(
-        address userAddress,
-        uint256 time,
-        string memory date
+    function createCheckOut(
+        address _userAddress,
+        uint256 _time,
+        string memory _date
     ) public {
-        Access memory exit = Access({userAddress: userAddress, time: time});
-        exits[date].push(exit);
+        Access memory checkOut = Access({
+            userAddress: _userAddress,
+            time: _time
+        });
+        checkOuts[_date].push(checkOut);
     }
 
-    function GetStudentEntries(string memory date)
+    function getCheckIns(string memory _date)
         public
         view
         returns (Access[] memory)
     {
-        return entries[date];
+        return checkIns[_date];
     }
 
-    function GetStudentExits(string memory date)
+    function getCheckOuts(string memory _date)
         public
         view
         returns (Access[] memory)
     {
-        return exits[date];
+        return checkOuts[_date];
     }
 }
