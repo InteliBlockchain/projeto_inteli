542fed501eb47eec14145eccf6e22a0ddff2016b
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2022-08-07 19:11:55 -0300

    Função viewLectures criadas

--- ethereum/contracts/LectureFactory.sol
+++ ethereum/contracts/LectureFactory.sol
@@ -1,26 +1,29 @@
-//SPDX-License-Identifier: GPL-3.0 
+//SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
 import "./Lecture.sol";
 
-contract LectureFactory{
-    
+contract LectureFactory {
     address owner;
-   
-    struct LectureStruct{
+
+    struct LectureStruct {
         string name;
         address endereco;
     }
 
     LectureStruct[] public lectures;
 
-    constructor () {
+    constructor() {
         owner = msg.sender;
     }
 
-    function createLecture(string memory _lectureName,address[] memory _arrayUsers, string memory _ipfsAddress) public returns(address){
+    function createLecture(
+        string memory _lectureName,
+        address[] memory _arrayUsers,
+        string memory _ipfsAddress
+    ) public returns (address) {
         require(msg.sender == owner);
-        Lecture mintLecture = new Lecture (_arrayUsers, _ipfsAddress);
+        Lecture mintLecture = new Lecture(_arrayUsers, _ipfsAddress);
 
         LectureStruct memory newLecture = LectureStruct({
             name: _lectureName,
@@ -29,7 +32,11 @@ contract LectureFactory{
 
         lectures.push(newLecture);
 
-        return(address(mintLecture));
+        return (address(mintLecture));
     }
 
+    function viewLectures() public view returns (LectureStruct[] memory) {
+        require(msg.sender == owner);
+        return (lectures);
+    }
 }
