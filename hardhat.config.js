273b4a16157cf517eb20cdc6d43cc5806b120023
Lemos1347 <henrique.matias@sou.inteli.edu.br>
2022-08-10 13:41:41 -0300

    Configuração do compilador do hardhat feito

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.9",
+      },
+      {
+        version: "0.8.15",
+      }
+    ],
+  },
+  paths: {
+    sources: "./ethereum/contracts",
+    artifacts: "./ethereum/artifacts",
+    // cache: "./ethereum/artifacts/cache"
+  }
+};
