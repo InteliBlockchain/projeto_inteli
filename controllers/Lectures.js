03ee05074f0a84b1c9a1bef0389f27c50d2db8ea
Lyorrei Shono Quintão <44589251+lyorrei@users.noreply.github.com>
2022-08-13 16:23:47 -0300

    Revert "Mudei nome de pastas e arquivos"

--- controllers/Lectures.js
+++ controllers/Lectures.js
@@ -1,5 +1,5 @@
 const { validationResult } = require('express-validator')
-const lectureService = require('../services/lecture')
+const lectureService = require('../services/Lecture')
 require('express-async-errors')
 
 const Lecture = new lectureService.Student()
