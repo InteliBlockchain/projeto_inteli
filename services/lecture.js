fad1f944e1d380a2475d9a8fb3431327a6505365
vict0rcarvalh0 <victordecarvalho@outlook.pt>
2022-09-23 21:02:05 -0300

    Comment patternering on the services  files

--- services/lecture.js
+++ services/lecture.js
@@ -1,8 +1,17 @@
+<<<<<<< Updated upstream
 const {
     lectureFactory,
     person,
     lecture,
 } = require('../utils/ethers')
+=======
+const { web3 } = require('../ethereum/utils/web3')
+// Compiled smart contract
+const { instance: lectureFactory } = require('../ethereum/contractsInteractions/lectureFactory')
+const { instance: inteliFactory } = require('../ethereum/contractsInteractions/inteliFactory')
+const { instance: person } = require('../ethereum/contractsInteractions/person')
+const { instance: lecture } = require('../ethereum/contractsInteractions/lecture')
+>>>>>>> Stashed changes
 
 const { createHash } = require('crypto')
 
@@ -11,9 +20,16 @@ const { storeNFT } = require('../ethereum/apis/nftStorage')
 
 class Lecture {
     async createLecture(file, lectureName, ras, description) {
+<<<<<<< Updated upstream
         const inteliFactoryInstance = await inteliFactory()
         const lectureFactoryInstance = await lectureFactory()
 
+=======
+        // Get the Inteli wallet
+        const accounts = await web3.eth.getAccounts()
+
+        // Get of all wallets from the RAs Array
+>>>>>>> Stashed changes
         const wallets = []
         const rasWithoutWallet = []
 
@@ -21,51 +37,58 @@ class Lecture {
             // Get de todas as wallets a partir do Array da Ras
             const wallet = await inteliFactoryInstance.getWallet(ras[i])
 
-            // Checar se a wallet existe
+            // Check if the wallet exists
             if (wallet != '0x0000000000000000000000000000000000000000') {
-                // Adicionar a wallet ao array
+                // Add the wallet to the array
                 wallets.push(wallet)
             } else {
                 rasWithoutWallet.push(ras[i])
             }
         }
-        // caso algum dos RAs passados não corresponda a uma carteira:
+        // If any of the RAs doesn't match a wallet
         if (rasWithoutWallet.length > 0) {
             const formatedRasWithoutWallet = rasWithoutWallet.join(', ')
             throw new Error("Erro! Não encontradas carteiras para os seguintes R.A's: " + formatedRasWithoutWallet)
         }
 
-        // Conectar ao banco de dados
+        // Connect to Database
         const db = await connectToDatabase()
 
-        // Hashear lecture name
+        // Hash lecture name
         const nameHash = createHash('sha256').update(lectureName).digest('hex')
 
-        // Armazenar no banco de dados
+        // Store in Database
         await db.run(`INSERT INTO lecture (name, nameHash) VALUES ('${lectureName}', '${nameHash}')`)
 
-        // Fechar o banco de dados
+        // Close the Database
         await db.close()
 
-        // Criar nova NFT no NFT Storage (devolve url)
+        // Create a new NFT on NFT Storage (return url)
         const result = await storeNFT(file, nameHash, description)
         const parsedResult = JSON.parse(JSON.stringify(result))
 
-        // Get da url e executa função createLecture do contrato LectureFactory
+        // Get from the url and execute the createLecture function of the LectureFactory contract
         const newLectureAddress = JSON.parse(
             JSON.stringify(
                 await lectureFactoryInstance.createLecture(wallets, parsedResult.url).events.NewLecture.returnValues
             )
         )[0]
 
+<<<<<<< Updated upstream
         // Executa a função newActivity do contrato Person para cada usuário que foi na palestra
         for (let i = 0; i < ras.length; i++) {
             const personInstance = await person(ras[i])
             await personInstance.newActivity('lecture', newLectureAddress)
+=======
+        // Execute the newActivity function of the Person contract for each user that went to the event
+        for (let i = 0; i < wallets.length; i++) {
+            await person(wallets[i]).methods.newActivity('lecture', newLectureAddress).send({ from: accounts[0] })
+>>>>>>> Stashed changes
         }
     }
 
     async getLecturesStudent(ra) {
+<<<<<<< Updated upstream
         const inteliFactoryInstance = await inteliFactory()
         const personInstance = await person(ra)
         // Conectar ao banco de dados
@@ -73,13 +96,31 @@ class Lecture {
 
         // Executa a função getWallet do contrato inteliFactory
         const wallet = await inteliFactoryInstance.getWallet(ra)
+=======
+        const accounts = await web3.eth.getAccounts()
+
+        // Connect to the Database
+        const db = await connectToDatabase()
+
+        // Execute the getWallet function of the contract inteliFactory
+        const wallet = await inteliFactory.methods.getWallet(ra).call({
+            from: accounts[0],
+        })
+>>>>>>> Stashed changes
 
         if (wallet == '0x0000000000000000000000000000000000000000') {
             throw new Error('Wallet não encontrada para esse RA')
         }
 
+<<<<<<< Updated upstream
         // Executa a função getActivities do contrato Person
         const lecturesAdresses = await personInstance.getActivities('lecture')
+=======
+        // Execute the getActivities function of the Person contract
+        const lecturesAdresses = await person(wallet).methods.getActivities('lecture').call({
+            from: accounts[0],
+        })
+>>>>>>> Stashed changes
 
         const lecturesMetadata = []
 
@@ -100,15 +141,19 @@ class Lecture {
             }
         }
 
-        // Fechar o banco de dados
+        // Close the Database
         await db.close()
 
         return lecturesMetadata
     }
 
     async getLectures() {
+<<<<<<< Updated upstream
         const lectureFactoryInstance = await lectureFactory()
         // Conectar ao banco de dados
+=======
+        // Connect to Database
+>>>>>>> Stashed changes
         const db = await connectToDatabase()
 
         const lectures = await lectureFactoryInstance.viewLectures()
@@ -141,7 +186,7 @@ class Lecture {
             }
         }
 
-        // Fechar o banco de dados
+        // Close the Database
         await db.close()
 
         return lecturesMetadata
--- services/student.js
+++ services/student.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 //compiled smart contract
 const abi = require('../build/contracts/InteliFactory.json')
 const { ethers } = require('ethers')
@@ -12,8 +13,21 @@ const { inteliFactory, accessCampus, person, lecture, lectureFactory } = require
 // Import novas validacoes
 const { walletDoesNotExistsValidation, studentDoesNotExistsValidation } = require('../utils/validation')
 const { walletExistsValidation } = require('../utils/validation')
+=======
+const { web3 } = require('../ethereum/utils/web3')
+// Compiled smart contract
+const { instance: inteliFactory } = require('../ethereum/contractsInteractions/inteliFactory')
+const { instance: person } = require('../ethereum/contractsInteractions/person')
+const { instance: accessCampus } = require('../ethereum/contractsInteractions/accessCampus')
+
+const structDecoder = require('../ethereum/utils/structDecoder')
+
+// Get deployed contracts adresses object
+const contractsAdresses = require('../ethereum/utils/contractsAddresses.json')
+>>>>>>> Stashed changes
 
 class Student {
+    // Get students from the wallet adress and return RA from the account
     async getStudent(address) {
         const instance = await inteliFactory()
         const student = await instance.getStudent(address)
@@ -21,29 +35,76 @@ class Student {
         return student
     }
 
+    // Get the wallet adress from RA
     async getWallet(ra) {
+<<<<<<< Updated upstream
         const instance = await inteliFactory()
         const wallet = await instance.getWallet(ra)
         walletDoesNotExistsValidation(wallet)
+=======
+        const accounts = await web3.eth.getAccounts()
+        const wallet = await inteliFactory.methods.getWallet(ra).call({ from: accounts[0] })
+        
+        // Check if the wallet gotten from RA exists
+        if (wallet === '0x0000000000000000000000000000000000000000') {
+            throw new Error(`Estudante com este RA ${ra} não encontrado`)
+        }
+        
+>>>>>>> Stashed changes
         return wallet
     }
 
+    // Create a student from the RA
     async createStudent(ra) {
+<<<<<<< Updated upstream
         const instance = await inteliFactory()
         const wallet = await instance.getWallet(ra)
         walletExistsValidation(wallet, ra)
         await instance.createStudent(ra)
+=======
+        const accounts = await web3.eth.getAccounts()
+
+        const wallet = await inteliFactory.methods.getWallet(ra).call({ from: accounts[0] })
+
+        // Check if the wallet exists
+        if (wallet !== '0x0000000000000000000000000000000000000000') {
+            throw new Error(`Carteira já existente para RA ${ra}`)
+        }
+
+        // Create the student
+        await inteliFactory.methods.createStudent(ra).send({
+            from: accounts[0],
+        })
+>>>>>>> Stashed changes
     }
 
+    // Remove a student from RA and erase his wallet
     async removeStudent(ra) {
+<<<<<<< Updated upstream
         const inteliFactoryInstance = await inteliFactory()
         await inteliFactoryInstance.removeStudent(ra)
 
         const personInstance = await person(ra)
         await personInstance.eraseMe()
+=======
+        // Lembrar de executar o autodestruct do contrato person (branch do lemos)
+        // Get the student wallet from RA
+        const accounts = await web3.eth.getAccounts()
+        const walletAddress = await this.getWallet(ra)
+
+        // Remove the student
+        await inteliFactory.methods.removeStudent(ra).send({
+            from: accounts[0],
+        })
+        await person(walletAddress).methods.eraseMe().send({
+            from: accounts[0],
+        })
+>>>>>>> Stashed changes
     }
 
+    // Register check in of a student in a specific date and time
     async checkIn(ra, time, date) {
+<<<<<<< Updated upstream
         const personInstance = await person(ra)
         await personInstance.registerCheckIn(date, time)
 
@@ -51,9 +112,24 @@ class Student {
         const accessCampusInstance = await accessCampus()
         await accessCampusInstance.registerCheckIn(wallet, time, date)
 
+=======
+        // Get the student wallet from RA
+        const accounts = await web3.eth.getAccounts()
+        const wallet = await this.getWallet(ra)
+
+        // Register the check in
+        await accessCampus.methods.registerCheckIn(wallet, time, date).send({
+            from: accounts[0],
+        })
+        await person(wallet).methods.registerCheckIn(date, time).send({
+            from: accounts[0],
+        })
+>>>>>>> Stashed changes
     }
 
+    // Register check out of a student in a specific date and time
     async checkOut(ra, time, date) {
+<<<<<<< Updated upstream
         const personInstance = await person(ra)
         await personInstance.registerCheckOut(date, time)
 
@@ -62,16 +138,45 @@ class Student {
         const accessCampusInstance = await accessCampus()
         await accessCampusInstance.registerCheckOut(wallet, time, date)
         
+=======
+        // Get the student wallet from RA
+        const accounts = await web3.eth.getAccounts()
+        const wallet = await this.getWallet(ra)
+
+        // Register the check out
+        await accessCampus.methods.registerCheckOut(wallet, time, date).send({
+            from: accounts[0],
+        })
+        await person(wallet).methods.registerCheckOut(date, time).send({
+            from: accounts[0],
+        })
+>>>>>>> Stashed changes
     }
 
+    // Get all the check ins of a student returning all the check in times
     async accesses(ra, date) {
+<<<<<<< Updated upstream
         const personInstance = await person(ra)
         const accesses = await personInstance.getCheckIn(date)
         const formatedAccesses = accesses.map(bigNumber => ethers.BigNumber.from(bigNumber).toNumber())
         return formatedAccesses
+=======
+        // Get the student wallet from RA
+        const accounts = await web3.eth.getAccounts()
+        const wallet = await this.getWallet(ra)
+
+        // Get all the check ins of the student
+        const times = await person(wallet).methods.getCheckIn(date).call({
+            from: accounts[0],
+        })
+
+        return times
+>>>>>>> Stashed changes
     }
 
+    // Get all the check outs of a student returning all the check out times
     async exits(ra, date) {
+<<<<<<< Updated upstream
         const personInstance = await person(ra)
         const exits = await personInstance.getCheckOut(date)
         const formatedExits = exits.map(bigNumber => ethers.BigNumber.from(bigNumber).toNumber())
@@ -79,6 +184,20 @@ class Student {
         return formatedExits
     }
     
+=======
+        // Get the student wallet from RA
+        const accounts = await web3.eth.getAccounts()
+        const wallet = await this.getWallet(ra)
+
+        // Get all the check outs of the student
+        const times = await person(wallet).methods.getCheckOut(date).call({
+            from: accounts[0],
+        })
+        return times
+    }
+
+    // Get all the check ins in a specific date returning all the adresses in that date
+>>>>>>> Stashed changes
     async allAccesses(date) {
         const accessCampusInstance = await accessCampus()
         const inteliFactoryInstance = await inteliFactory()
@@ -105,6 +224,7 @@ class Student {
         return decodedStructs
     }
 
+    // Get all the check outs in a specific date returning all the adresses in that date
     async allExits(date) {
         const accessCampusInstance = await accessCampus()
         const inteliFactoryInstance = await inteliFactory()
@@ -131,35 +251,71 @@ class Student {
         return wallets
     }
 
+    // Get the balance of a specific wallet
     async balance(ra) {
+<<<<<<< Updated upstream
         const personInstance = await person(ra)
         const balance = await personInstance.getBalance()
+=======
+        // Get the student wallet from RA
+        const accounts = await web3.eth.getAccounts()
+        const wallet = await this.getWallet(ra)
+
+        // Get the balance
+        const balance = await person(wallet).methods.getBalance().call({
+            from: accounts[0],
+        })
+>>>>>>> Stashed changes
 
         return balance
     }
 
+    // Transfer a quantity of money from a wallet to another
     async transferMoney(from, quantity, to) {
+<<<<<<< Updated upstream
         const personInstance = await person(from)
 
+=======
+        // Get the sender student wallet
+        const accounts = await web3.eth.getAccounts()
+>>>>>>> Stashed changes
         const fromWallet = await this.getWallet(from)
 
         let toWallet = null
+        // Get the receiver student wallet
         if (to == 'Inteli') {
             toWallet = process.env.BLOCKCHAIN_ACCOUNT_ADDRESS
         } else {
             toWallet = await this.getWallet(to)
             walletDoesNotExistsValidation(toWallet)
         }
+<<<<<<< Updated upstream
         
         if (quantity === '0') {
             throw new Error('Quantidade inválida para transferência')
+=======
+
+        // Check if the quantity is null and throw an error
+        if (quantity === "0") {
+            throw new Error("Quantidade inválida para transferência")
+
+>>>>>>> Stashed changes
         }
 
+        // Check if the receiver wallet is the same as the sender one and throw an error
         if (toWallet === fromWallet) {
             throw new Error('Carteiras iguais, transferência inválida')
         }
+<<<<<<< Updated upstream
 
         await personInstance.transferMoney(toWallet, quantity)
+=======
+        
+        // Transfer the money
+        await person(fromWallet).methods.transferMoney(toWallet, quantity).send({
+            from: accounts[0],
+        })
+>>>>>>> Stashed changes
     }
 }
 
